fb6e7b4501fa3ea9f3f909705699704e0fc86a17
manateemilitia <brandon.block@gmail.com>
2016-02-10 23:16:48 -0500

    Implement geopy to find coords from zip

--- watttext.py
+++ watttext.py
@@ -2,6 +2,13 @@ import twilio
 import requests
 import json
 
+from geopy.geocoders import Nominatim
+
+
+watttime_token = '062fcb6e4c2eac88241e0a727f2cf4fb8c1cca29'
+twilio_id = 'AC1773d28a4edb400cd3a97a854ded20e1'
+twilio_token = 'fb67c60d1088d5b503b0ef1e00ab526c'
+
 
 def receive_text(text):
     # Receives and parses text into a zip code.
@@ -11,8 +18,12 @@ def receive_text(text):
 
 def zip_lookup(zip):
     # Take zip code and find coords.
-    long = ""
-    lat = ""
+    geolocator = Nominatim()
+    location = geolocator.geocode(zip)
+
+    lat = location.latitude
+    long = location.longitude
+
     return long, lat
 
 
@@ -30,3 +41,10 @@ def get_mix_data(ba):
 def text_data(mix_data):
     # Texts the mix data out via Twilio
     pass
+
+
+def text_sorry():
+    pass
+
+
+print(zip_lookup("27514"))

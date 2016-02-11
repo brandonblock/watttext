b9c4d4cc55d5e0b5abe094e1758fbfbc163539ce
manateemilitia <brandon.block@gmail.com>
2016-02-10 23:51:32 -0500

    Implement loc_to_ba to take coords and return balancing authority

--- watttext.py
+++ watttext.py
@@ -1,11 +1,11 @@
-import twilio
+# import twilio
 import requests
 import json
 
 from geopy.geocoders import Nominatim
 
-
-watttime_token = '062fcb6e4c2eac88241e0a727f2cf4fb8c1cca29'
+watttime_token = 'Token 062fcb6e4c2eac88241e0a727f2cf4fb8c1cca29'
+headers = {'Authorization': watttime_token}
 twilio_id = 'AC1773d28a4edb400cd3a97a854ded20e1'
 twilio_token = 'fb67c60d1088d5b503b0ef1e00ab526c'
 
@@ -21,19 +21,24 @@ def zip_lookup(zip):
     geolocator = Nominatim()
     location = geolocator.geocode(zip)
 
-    lat = location.latitude
-    long = location.longitude
+    lat = str(location.latitude)
+    long = str(location.longitude)
 
-    return long, lat
+    return [long, lat]
 
 
-def loc_to_ba(long, lat):
-    ba = ""
-    return ba
+def loc_to_ba(coords):
+    url = 'https://api.watttime.org/api/v1/balancing_authorities/?loc={"type":"Point","coordinates":[%s,%s]}' % (coords[0], coords[1])
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    resultsjson = json.loads(r.text)
+    abbrev = resultsjson[0]['abbrev']
+    return abbrev
 
 
 def get_mix_data(ba):
     # Takes the balancing authority and returns mix data.
+    url = ""
     mix_data = {}
     return mix_data
 
@@ -47,4 +52,4 @@ def text_sorry():
     pass
 
 
-print(zip_lookup("27514"))
+get_mix_data(loc_to_ba(zip_lookup("27514")))
